eae0b9bbaecad78b70ac5685edd56b04371187e8
MUHAMMAD ASIF <162907407+Muhammadasif1986@users.noreply.github.com>
2025-01-19 22:11:27 +0500

    day-3
    day-3

--- my-app/app/components/FoodCategory.tsx
+++ my-app/app/components/FoodCategory.tsx
@@ -1,21 +1,50 @@
+'use client'
 import Image from "next/image";
 import { Great_Vibes } from "next/font/google";
+import { client } from "@/sanity/lib/client";
+import { useEffect , useState } from "react";
 const Vibes = Great_Vibes({
       subsets: ["latin"],
       weight: "400",
     });
 
+    interface foodImage {
+      imageUrl: string;
+    }
+
+    const getData = async (): Promise<foodImage[]> => {
+ 
+      const data = await client.fetch(
+           `*[_type == "food"]{
+          
+          "imageUrl": image.asset->url
+        }`
+       );
+     return data;
+   };
+
 export default function Cart() {
+  const [FoodImage, setFoodImage] = useState<foodImage[]>([]);
+
+  useEffect(() => {
+    // Fetch the product data on component mount
+    const fetchFoodImages = async () => {
+      const data = await getData();
+      setFoodImage(data);
+    };
+    fetchFoodImages();
+  }, []);
   return (
-    <main className="bg-black flex w-full h-auto justify-center items-center py-12">
+    <main className="bg-black flex w-full justify-center items-center py-12 h-auto">
       <div className="flex flex-col justify-center items-center">
             <div className={Vibes.className}><p className="text-[#ff9f0d] text-2xl mb-4">Food Category</p></div>
             <div className="text-[#ff9f0d] text-2xl mb-8 flex">Ch<p className="text-white">oose Your Food Items </p></div>
-            <div className="flex flex-col lg:flex-row justify-between gap-6 items-center h-auto lg:h-[200px]">
-                  <div><Image src="/unsplash7.png" alt="Image" width={200} height={200} /></div>
-                  <div><Image src="/unsplash8.png" alt="Image" width={200} height={200} /></div>
-                  <div><Image src="/unsplash9.png" alt="Image" width={200} height={200} /></div>
-                  <div><Image src="/unsplash10.png" alt="Image" width={200} height={200} /></div>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 justify-between gap-4 items-center h-auto lg:h-[200px]">
+               {FoodImage.map((food, index) => (
+                  <div key={index}>
+                    <Image src={food.imageUrl} alt="food" width={200} height={200} />
+                  </div>
+               ))}
             </div>
       </div>
     </main>
--- my-app/app/components/OurChef/OurChefDetails.tsx
+++ my-app/app/components/OurChef/OurChefDetails.tsx
@@ -1,35 +1,56 @@
+'use client'
+
 import Image from "next/image";
+import {client} from '@/s anity/lib/client';
+import { useEffect, useState } from 'react';
+import Link from "next/link"
+import { Check } from "lucide-react";
+
+interface chefData {
+  name: string;
+  position: string;
+  imageUrl: string;
+}
+
+const getData = async (): Promise<chefData[]> => {
+
+  const data = await client.fetch(
+       `*[_type == "chef"]{
+      name,
+      position,
+      "imageUrl": image.asset->url
+    }`
+   );
+ return data;
+};
 
 export default function OurChefDetails() {
-  const teamMembers = [
-    { name: "Tahmina Rumi", role: "Chef", image: "/chef.png" },
-    { name: "Jorina Begum", role: "Chef", image: "/chef1.png" },
-    { name: "M. Mohammad", role: "Chef", image: "/chef2.png" },
-    { name: "Munna Kathy", role: "Chef", image: "/chef3.png" },
-    { name: "Tahmina Rumi", role: "Chef", image: "/chef4.png" },
-    { name: "Bisnu Devgon", role: "Chef", image: "/member.png" },
-    { name: "Motin Molladf", role: "Chef", image: "/chef5.png" },
-    { name: "William Rumi", role: "Chef", image: "/chef6.png" },
-    { name: "Kets William Roy", role: "Chef", image: "/chef7.png" },
-    { name: "Mahmud Kholil", role: "Chef", image: "/chef8.png" },
-    { name: "Ataur Rahman", role: "Chef", image: "/chef9.png" },
-    { name: "Monalisa Holly", role: "Chef", image: "/chef10.png" },
-  ];
 
+   const [ChefData, setChefData] = useState<chefData[]>([]);
+      
+        useEffect(() => {
+          const fetchChefData = async () => {
+            const data = await getData();
+            setChefData(data);
+          };
+          fetchChefData();
+        }, []);
+ 
   return (
     <div className="max-w-screen-xl mx-auto p-4 lg:w-8/12 py-10 ">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-        {teamMembers.map((member, index) => (
+        <Link href={`${ChefData.name}`}></Linkk>
+        {ChefData.map((member, index) => (
           <div key={index} className="text-center shadow-lg rounded-lg pb-4">
             <Image
-              src={member.image}
+              src={member.imageUrl}
               alt={member.name}
               width={300}
               height={300}
               className="rounded-lg object-cover"
             />
             <h2 className="font-semibold mt-4 text-center">{member.name}</h2>
-            <p className="text-gray-600 text-center">{member.role}</p>
+            <p className="text-gray-600 text-center">{member.position}</p>
           </div>
         ))}
       </div>
